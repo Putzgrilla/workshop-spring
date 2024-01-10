d1a733c8b61cc2be1e91225c678c186ed401d900
arthur <arthurmariano2003@gmail.com>
2024-01-10 15:08:41 -0300

    primeiro

---
+++ src/main/java/com/Arthur/Projetospring/Entities/User.java
@@ -0,0 +1,85 @@
+package com.Arthur.Projetospring.Entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String senha;
+
+	public User() {
+
+	}
+
+	public User(long id, String nome, String email, String telefone, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.senha = senha;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+}
