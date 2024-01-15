adaf507891927d03531add9747da12957c00b835
arthur <arthurmariano2003@gmail.com>
2024-01-15 01:21:33 -0300

    4

---
+++ src/main/java/com/Arthur/Projetospring/UserServices/UserService.java
@@ -0,0 +1,27 @@
+package com.Arthur.Projetospring.UserServices;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Arthur.Projetospring.Entities.User;
+import com.Arthur.Projetospring.Repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+private UserRepository repository; 
+	public List<User> findAll(){
+		
+		return repository.findAll();
+	}
+	public User FindById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+		
+	}
+
+}
--- src/main/java/com/Arthur/Projetospring/resources/UserResource.java
+++ src/main/java/com/Arthur/Projetospring/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.Arthur.Projetospring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Arthur.Projetospring.Entities.User;
+import com.Arthur.Projetospring.UserServices.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 
 public class UserResource {
+	@Autowired
+	private UserService service;
 	@GetMapping
-	public ResponseEntity<User> procurarTudo() {
-		User u = new User(1L, "jonas", "jonas@gmail.com", "13887403312", "12345");
-		return ResponseEntity.ok().body(u);
-
+	public ResponseEntity<List<User>> procurarTudo() {
+		List<User> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
 	}
+@GetMapping(value = "/{id}")
+public ResponseEntity<User> findbyId(@PathVariable Long id){
+	User obj = service.FindById(id);
+	
+	return ResponseEntity.ok().body(obj);
+	
+}
 }
