79212823e40169a4ddba5f9908695e7eb85edda5
arthur <arthurmariano2003@gmail.com>
2024-01-23 03:39:43 -0300

    10

--- src/main/java/com/Arthur/Projetospring/Services/UserService.java
+++ src/main/java/com/Arthur/Projetospring/Services/UserService.java
@@ -12,20 +12,37 @@ import com.Arthur.Projetospring.Repositories.UserRepository;
 @Service
 public class UserService {
 	@Autowired
-private UserRepository repository; 
-	public List<User> findAll(){
-		
+	private UserRepository repository;
+
+	public List<User> findAll() {
+
 		return repository.findAll();
 	}
+
 	public User FindById(long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
-		
-		
-	}
-	public User insert(User obj)
-	{
-	return repository.save(obj);	
+
 	}
 
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	public User upadate(Long id, User obj) 
+	{
+		User entity = repository.getReferenceById(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		// TODO Auto-generated method stub
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setphone(obj.getphone());
+	}
 }
--- src/main/java/com/Arthur/Projetospring/resources/UserResource.java
+++ src/main/java/com/Arthur/Projetospring/resources/UserResource.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.Arthur.Projetospring.Entities.User;
 import com.Arthur.Projetospring.Services.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -23,24 +25,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserResource {
 	@Autowired
 	private UserService service;
+
 	@GetMapping
 	public ResponseEntity<List<User>> procurarTudo() {
 		List<User> lista = service.findAll();
 		return ResponseEntity.ok().body(lista);
 	}
-@GetMapping(value = "/{id}")
-public ResponseEntity<User> findbyId(@PathVariable Long id){
-	User obj = service.FindById(id);
-	
-	return ResponseEntity.ok().body(obj);
-	
-}
-@PostMapping()
-public ResponseEntity<User> insert(@RequestBody User obj) {
-    //TODO: process POST request
-    obj= service.insert(obj);
-    URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-	return ResponseEntity.created(uri).body(obj);
-}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findbyId(@PathVariable Long id) {
+		User obj = service.FindById(id);
+
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+	@PostMapping()
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		// TODO: process POST request
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id,  @RequestBody User obj) {
+		//TODO: process PUT request
+		obj = service.upadate(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
